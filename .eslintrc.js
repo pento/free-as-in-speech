b754564fd6503fcdfcca32d7d96439c0f92cdecf
Gary <gary@pento.net>
2021-01-14 12:20:28 +1100

    Add some comments, and tweak the eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,8 @@
 const eslintConfig = {
 	root: true,
-	extends: [
-		'plugin:@wordpress/eslint-plugin/recommended-with-formatting',
-	],
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
 	globals: {
 		browser: 'readonly',
-	},
-	rules: {
-		'jsdoc/require-jsdoc': 'error',
-		'jsdoc/require-param-description': 'error',
-		'jsdoc/require-returns': 'error',
 	},
 };
 
--- source/background.js
+++ source/background.js
@@ -6,5 +6,6 @@ import WXR from '@wordpress/wxr';
 browser.pageAction.onClicked.addListener( () => {
 	const exporter = new WXR();
 
+	// eslint-disable-next-line no-console
 	console.log( exporter.export() );
 } );
