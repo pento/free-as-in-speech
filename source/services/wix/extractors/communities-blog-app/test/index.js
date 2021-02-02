2dbb56845a0e90724bb8f605921484523876339a
Alex Kirk <alex.git@kirk.at>
2021-02-02 15:04:11 +0100

    Mock extracting the communities-blog-app

---
+++ source/services/wix/extractors/communities-blog-app/test/index.js
@@ -0,0 +1,49 @@
+const fetchMock = require( 'fetch-mock-jest' );
+const index = require( '../index' );
+
+
+
+const data = [{
+	posts: [{
+		owner: 1,
+	}],
+	authors: [{
+		siteMemberId: 1,
+		name: 'bob'
+	}],
+	tags: [],
+	categories: [],
+	extracted: {},
+}
+];
+data[0].extracted.posts = data[0].posts;
+data[0].extracted.authors = data[0].authors;
+data[0].extracted.categories = data[0].categories;
+data[0].extracted.tags = data[0].tags;
+
+test.each(data)( 'extract posts', async ( data ) => {
+	fetchMock
+		.mock( {
+			matcher: 'begin:https://manage.wix.com/_api/communities-blog-node-api/_api/posts',
+			query: { status: 'published' },
+			method: 'get',
+			response: JSON.stringify( data.posts )
+		})
+		.mock( {
+			matcher: 'https://manage.wix.com/_serverless/assignee-service/assignees',
+			method: 'get',
+			response: JSON.stringify( { assignees: data.authors } )
+		})
+		.mock( {
+			matcher: 'https://manage.wix.com/_api/communities-blog-node-api/v2/tags/query',
+			method: 'post',
+			response: JSON.stringify( { tags: data.tags } )
+		})
+		.mock( '*', { body: '[]' } );
+
+	return index.settings.extract( {
+		instance: 'test',
+	} ).then( result => {
+		expect( result ).toEqual( data.extracted );
+	});
+});
