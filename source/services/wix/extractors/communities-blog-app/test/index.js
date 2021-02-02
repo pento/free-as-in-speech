7ea0271f532a4d46a906791149d2b64aed947cf3
Alex Kirk <alex.git@kirk.at>
2021-02-02 15:17:05 +0100

    Lint fixes

--- source/services/wix/extractors/communities-blog-app/test/index.js
+++ source/services/wix/extractors/communities-blog-app/test/index.js
@@ -1,49 +1,57 @@
 const fetchMock = require( 'fetch-mock-jest' );
 const index = require( '../index' );
 
-
-
-const data = [{
-	posts: [{
-		owner: 1,
-	}],
-	authors: [{
-		siteMemberId: 1,
-		name: 'bob'
-	}],
-	tags: [],
-	categories: [],
-	extracted: {},
-}
+const data = [
+	{
+		posts: [
+			{
+				owner: 1,
+			},
+		],
+		authors: [
+			{
+				siteMemberId: 1,
+				name: 'bob',
+			},
+		],
+		tags: [],
+		categories: [],
+		extracted: {},
+	},
 ];
-data[0].extracted.posts = data[0].posts;
-data[0].extracted.authors = data[0].authors;
-data[0].extracted.categories = data[0].categories;
-data[0].extracted.tags = data[0].tags;
+data[ 0 ].extracted.posts = data[ 0 ].posts;
+data[ 0 ].extracted.authors = data[ 0 ].authors;
+data[ 0 ].extracted.categories = data[ 0 ].categories;
+data[ 0 ].extracted.tags = data[ 0 ].tags;
 
-test.each(data)( 'extract posts', async ( data ) => {
+test.each( data )( 'extract posts', async ( testData ) => {
 	fetchMock
 		.mock( {
-			matcher: 'begin:https://manage.wix.com/_api/communities-blog-node-api/_api/posts',
+			matcher:
+				'begin:https://manage.wix.com/_api/communities-blog-node-api/_api/posts',
 			query: { status: 'published' },
 			method: 'get',
-			response: JSON.stringify( data.posts )
-		})
+			response: JSON.stringify( testData.posts ),
+		} )
 		.mock( {
-			matcher: 'https://manage.wix.com/_serverless/assignee-service/assignees',
+			matcher:
+				'https://manage.wix.com/_serverless/assignee-service/assignees',
 			method: 'get',
-			response: JSON.stringify( { assignees: data.authors } )
-		})
+			response: JSON.stringify( { assignees: testData.authors } ),
+		} )
 		.mock( {
-			matcher: 'https://manage.wix.com/_api/communities-blog-node-api/v2/tags/query',
+			matcher:
+				'https://manage.wix.com/_api/communities-blog-node-api/v2/tags/query',
 			method: 'post',
-			response: JSON.stringify( { tags: data.tags } )
-		})
+			response: JSON.stringify( { tags: testData.tags } ),
+		} )
 		.mock( '*', { body: '[]' } );
 
-	return index.settings.extract( {
-		instance: 'test',
-	} ).then( result => {
-		expect( result ).toEqual( data.extracted );
-	});
-});
+	return index.settings
+		.extract( {
+			instance: 'test',
+		} )
+		.then( ( result ) => {
+			expect( result ).toEqual( testData.extracted );
+		} );
+} );
