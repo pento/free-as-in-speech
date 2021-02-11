e036b053fe536fed3f3ae12aa11c504893b30252
Alex Kirk <alex.git@kirk.at>
2021-02-11 13:15:40 +0100

    lint fix

--- bin/export-from-har.js
+++ bin/export-from-har.js
@@ -57,39 +57,36 @@ program
 	.option( '-m, --mediaToken <mediaToken>', 'Specify a media token', null )
 	.arguments( '<harfile>', 'The file to import' )
 	.description( 'Extract from Wix' )
-	.action( ( harfile, options, command ) => {
-		window.fetch = fetchFromHAR(
-			JSON.parse( fs.readFileSync( harfile ) ),
-			{
-				queryComparison: ( requestValue, harValue, key, url ) => {
-					if ( requestValue === harValue ) {
-						return true;
-					}
-					if (
-						'undefined' !==
-						typeof mockMapping[ url.host ][ url.pathname ][ key ]
-					) {
-						return mockMapping[ url.host ][ url.pathname ][ key ];
-					}
-					return false;
-				},
-				fallback: ( url, entry ) => {
-					console.log( 'Not Found', url ); // eslint-disable-line no-console
-					const u = new URL( url );
-					entry.response.content.text = '[]';
-					// an example of how to hard-code a fallback, this will only be used if the HAR doesn't have such an entry.
-					if (
-						u.pathname ===
-						'/_api/communities-blog-node-api/v2/tags/query'
-					) {
-						entry.response.status = 200;
-						entry.response.statusText = 'OK';
-						entry.response.content.text = '{"tags":[]}';
-					}
-					return entry;
-				},
-			}
-		);
+	.action( ( harfile, options ) => {
+		window.fetch = fetchFromHAR( JSON.parse( fs.readFileSync( harfile ) ), {
+			queryComparison: ( requestValue, harValue, key, url ) => {
+				if ( requestValue === harValue ) {
+					return true;
+				}
+				if (
+					'undefined' !==
+					typeof mockMapping[ url.host ][ url.pathname ][ key ]
+				) {
+					return mockMapping[ url.host ][ url.pathname ][ key ];
+				}
+				return false;
+			},
+			fallback: ( url, entry ) => {
+				console.log( 'Not Found', url ); // eslint-disable-line no-console
+				const u = new URL( url );
+				entry.response.content.text = '[]';
+				// an example of how to hard-code a fallback, this will only be used if the HAR doesn't have such an entry.
+				if (
+					u.pathname ===
+					'/_api/communities-blog-node-api/v2/tags/query'
+				) {
+					entry.response.status = 200;
+					entry.response.statusText = 'OK';
+					entry.response.content.text = '{"tags":[]}';
+				}
+				return entry;
+			},
+		} );
 		async function getWxr() {
 			if ( typeof options.appDefinitionId === 'string' ) {
 				options.appDefinitionId = options.appDefinitionId.split( /,/ );
