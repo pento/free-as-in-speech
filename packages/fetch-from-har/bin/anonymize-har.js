657c65670505a241b27d0c289cb4e64d7ef2cf84
Alex Kirk <alex.git@kirk.at>
2021-03-04 17:05:24 +0100

    Move to the fetch-from-har package

--- packages/fetch-from-har/bin/anonymize-har.js
+++ packages/fetch-from-har/bin/anonymize-har.js
@@ -62,7 +62,8 @@ const valueRegex = ( key, flags ) => {
 
 let firstTime;
 const anonymizers = {
-	datetime: { // ISO 8601
+	datetime: {
+		// ISO 8601
 		regex: /\b((?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:[.]\d{1,5})?(?:Z|[+-][01]\d:[0-5]\d))/g,
 		replacement: ( m ) => {
 			if ( ! firstTime ) {
@@ -188,7 +189,11 @@ Object.entries( replacements ).forEach(
 
 // Display what was replaced.
 console.error( 'The following number of items were replaced:' );
-Object.entries( logReplacements ).forEach( ( category ) => Object.keys( category[1] ).length && console.log( category[0], Object.keys( category[1] ).length ) );
+Object.entries( logReplacements ).forEach(
+	( category ) =>
+		Object.keys( category[ 1 ] ).length &&
+		console.log( category[ 0 ], Object.keys( category[ 1 ] ).length )
+);
 console.error( logReplacements );
 
 const outfile = path.join(
