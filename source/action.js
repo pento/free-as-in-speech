f10b77b0a843ea8b9843f6ee902b86c6d27ee639
Gary <gary@pento.net>
2021-03-11 11:32:17 +1100

    Switch to using babel, htm had issues.

--- source/action.js
+++ source/action.js
@@ -1,6 +1,5 @@
 const ReactDOM = require( 'react-dom' );
 const { Component } = require( '@wordpress/element' );
-const { html } = require( 'htm/react' );
 
 class App extends Component {
 	constructor( props ) {
@@ -11,12 +10,10 @@ class App extends Component {
 		};
 	}
 	componentDidMount() {
-		console.log( 'lol' );
 		browser.tabs
 			.query( { active: true, currentWindow: true } )
 			.then( async ( tabs ) => {
 				const currentTabUrl = tabs[ 0 ].url;
-				console.log( tabs );
 
 				if (
 					currentTabUrl.startsWith(
@@ -43,54 +40,59 @@ class App extends Component {
 		} );
 	}
 
-	render( props, state ) {
-		console.log( { props, state } );
-		let page = '';
-		if ( state && state.page ) {
-			page = state.page;
-		}
+	render() {
+		const { page } = this.state;
+
 		switch ( page ) {
 			case 'wix-site':
-				return html`<div id="wix-content">
-					<p>You can export your site now!</p>
-					<p>
-						If you're ready to export this site, click the export
-						button.
-					</p>
-					<p>
-						<button
-							id="wix-export"
-							onClick={ this.startWixExport }
-						>
-							Export
-						</button>
-					</p>
-				</div>`;
+				return (
+					<div id="wix-content">
+						<p>You can export your site now!</p>
+						<p>
+							If you're ready to export this site, click the
+							export button.
+						</p>
+						<p>
+							<button
+								id="wix-export"
+								onClick={ this.startWixExport }
+							>
+								Export
+							</button>
+						</p>
+					</div>
+				);
 			case 'wix-site-no-config':
-				return html`<div id="wix-no-config">
-					<p>We were unable to retrieve Wix config data.</p>
+				return (
+					<div id="wix-no-config">
+						<p>We were unable to retrieve Wix config data.</p>
 
-					<p>Please refresh the page to try again.</p>
-				</div>`;
+						<p>Please refresh the page to try again.</p>
+					</div>
+				);
 			default:
-				return html`<div id="intro-content">
-					<p>
-						Welcome to WordPress'
-						<em>Free (as in Speech)</em> extension!
-					</p>
-					<p>
-						This extension helps you get control of your content
-						back from services that otherwise prevent you from
-						choosing to export the content that you own.
-					</p>
-					<p>Currently, <strong>Wix</strong> is supported.</p>
-					<p>
-						To get started, login to your account on Wix.com, then
-						click this button again!
-					</p>
-				</div>`;
+				return (
+					<div id="intro-content">
+						<p>
+							Welcome to WordPress'
+							<em>Free (as in Speech)</em> extension!
+						</p>
+						<p>
+							This extension helps you get control of your content
+							back from services that otherwise prevent you from
+							choosing to export the content that you own.
+						</p>
+						<p>
+							Currently, <strong>Wix</strong> is supported.
+						</p>
+						<p>
+							To get started, login to your account on Wix.com,
+							then click this button again!
+						</p>
+					</div>
+				);
 		}
 	}
 }
 
-ReactDOM.render( html`<${ App } />`, document.getElementById( 'root' ) );
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
