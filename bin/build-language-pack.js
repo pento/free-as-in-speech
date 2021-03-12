4c2c6e874477eb3cd62cd4e455f6c523bb30cb7e
Gary <gary@pento.net>
2021-03-12 15:31:10 +1100

    Language pack build runs before webpack, so we need to build the output directory.

--- bin/build-language-pack.js
+++ bin/build-language-pack.js
@@ -1,6 +1,6 @@
 const { parseFileSync } = require( 'po2json' );
 const glob = require( 'glob' );
-const { writeFileSync } = require( 'fs' );
+const { mkdirSync, writeFileSync } = require( 'fs' );
 
 const languageFiles = glob.sync( 'languages/*.po' );
 
@@ -14,4 +14,5 @@ languageFiles.forEach( ( file ) => {
 	} = ${ JSON.stringify( languageJson.locale_data.messages ) };\n`;
 } );
 
+mkdirSync( 'distribution/build', { recursive: true } );
 writeFileSync( 'distribution/build/language-pack.js', languagePackData );
