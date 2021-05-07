b2cc617bb1ab658c81c6e7094350cf04866c0eab
Alex Kirk <akirk@users.noreply.github.com>
2021-05-07 16:54:09 +0200

    Convert menu items navigation-link blocks

--- source/services/wix/extractors/static-pages/components/menu.js
+++ source/services/wix/extractors/static-pages/components/menu.js
@@ -1,12 +1,51 @@
 const { createBlock } = require( '@wordpress/blocks' );
+const { parseWixLink } = require( '../links.js' );
+
+const parseMenuRecursively = ( menuItem, resolver ) => {
+	menuItem = resolver( menuItem );
+	const attributes = {
+		label: menuItem.name || menuItem.label,
+	};
+	let innerBlocks = [];
+
+	if ( menuItem.link ) {
+		const parsedLink = parseWixLink( menuItem.link );
+		if ( 'post_type' === parsedLink.type ) {
+			attributes.type = parsedLink.object;
+			attributes.id = parsedLink.objectId;
+		}
+		attributes.url = parsedLink.url;
+	}
+
+	if ( menuItem.items && menuItem.items.length > 0 ) {
+		innerBlocks = menuItem.items.map( ( menuData ) =>
+			parseMenuRecursively( menuData, resolver )
+		);
+	}
+
+	return createBlock( 'core/navigation-link', attributes, innerBlocks );
+};
 
 module.exports = {
 	type: 'CustomMenuDataRef',
-	parseComponent: () => {
+	parseComponent: ( component, { resolver } ) => {
 		try {
-			return createBlock( 'core/navigation', {
-				orientation: 'horizontal',
-			} );
+			let menuItems = [];
+			if (
+				component.dataQuery.menuRef &&
+				component.dataQuery.menuRef.items
+			) {
+				menuItems = component.dataQuery.menuRef.items.map(
+					( menuData ) => parseMenuRecursively( menuData, resolver )
+				);
+			}
+			return createBlock(
+				'core/navigation',
+				{
+					orientation: 'horizontal',
+				},
+				menuItems
+			);
 		} catch ( e ) {
 			return null;
 		}
