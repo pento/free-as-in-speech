1d392e6729602c3b2d904b69c608f9d7be23b41a
Alex Kirk <akirk@users.noreply.github.com>
2021-05-07 17:31:20 +0200

    Hide invisible menu items, don't use post id data

--- source/services/wix/extractors/static-pages/components/menu.js
+++ source/services/wix/extractors/static-pages/components/menu.js
@@ -3,6 +3,9 @@ const { parseWixLink } = require( '../links.js' );
 
 const parseMenuRecursively = ( menuItem, resolver ) => {
 	menuItem = resolver( menuItem );
+	if ( ! menuItem.isVisible ) {
+		return null;
+	}
 	const attributes = {
 		label: menuItem.name || menuItem.label,
 	};
@@ -10,17 +13,22 @@ const parseMenuRecursively = ( menuItem, resolver ) => {
 
 	if ( menuItem.link ) {
 		const parsedLink = parseWixLink( menuItem.link );
-		if ( 'post_type' === parsedLink.type ) {
-			attributes.type = parsedLink.object;
-			attributes.id = parsedLink.objectId;
-		}
+		// We're not using this data for now since the post ids not being rewritten,
+		// resulting in the menu items to be hidden because these seemingly arbitrary
+		// post ids lead to posts that are not published.
+		//
+		// if ( 'post_type' === parsedLink.type ) {
+		// 	attributes.type = parsedLink.object;
+		// 	attributes.id = parsedLink.objectId;
+		// }
+
 		attributes.url = parsedLink.url;
 	}
 
 	if ( menuItem.items && menuItem.items.length > 0 ) {
-		innerBlocks = menuItem.items.map( ( menuData ) =>
-			parseMenuRecursively( menuData, resolver )
-		);
+		innerBlocks = menuItem.items
+			.map( ( menuData ) => parseMenuRecursively( menuData, resolver ) )
+			.filter( Boolean );
 	}
 
 	return createBlock( 'core/navigation-link', attributes, innerBlocks );
@@ -35,9 +43,11 @@ module.exports = {
 				component.dataQuery.menuRef &&
 				component.dataQuery.menuRef.items
 			) {
-				menuItems = component.dataQuery.menuRef.items.map(
-					( menuData ) => parseMenuRecursively( menuData, resolver )
-				);
+				menuItems = component.dataQuery.menuRef.items
+					.map( ( menuData ) =>
+						parseMenuRecursively( menuData, resolver )
+					)
+					.filter( Boolean );
 			}
 			return createBlock(
 				'core/navigation',
