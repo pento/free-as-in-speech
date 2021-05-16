7281a4cd992628e0718d3226d77a8dbefed215f6
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-05-16 17:03:34 +0200

    Register a new fetch-as-har package

---
+++ packages/fetch-as-har/src/index.js
@@ -0,0 +1,5 @@
+const staticPagesParserWix = () => {};
+
+module.exports = {
+	staticPagesParserWix,
+};
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
 		content: './source/content',
 		action: './source/action',
 		'@wordpress/wxr': './packages/wxr',
+		'fetch-as-har': './packages/fetch-as-har',
 		'fetch-from-har': './packages/fetch-from-har',
 	},
 	output: {
