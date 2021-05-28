81edbf18d9f1ddd8200a52d54ee6e6dab5c7e4e4
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-05-28 13:31:39 +0200

    Introduce TwitterFollow component mapper

--- packages/site-parsers/src/parsers/wix/mappers.js
+++ packages/site-parsers/src/parsers/wix/mappers.js
@@ -15,6 +15,7 @@ const componentHandlers = [
 	require( './components/menu.js' ),
 	require( './components/image.js' ),
 	require( './components/button.js' ),
+	require( './components/twitter-follow.js' ),
 ].reduce( handlerMapper( 'type' ), {} );
 
 const wrapResult = ( block, component ) => {
