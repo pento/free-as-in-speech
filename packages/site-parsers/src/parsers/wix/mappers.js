01d2b5e06c96bfedbdcb440eaba7624afbd82114
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-06-02 10:27:53 +0200

    Introduce Anchor component mapper

--- packages/site-parsers/src/parsers/wix/mappers.js
+++ packages/site-parsers/src/parsers/wix/mappers.js
@@ -15,6 +15,7 @@ const componentHandlers = [
 	require( './components/menu.js' ),
 	require( './components/image.js' ),
 	require( './components/button.js' ),
+	require( './components/anchor.js' ),
 ].reduce( handlerMapper( 'type' ), {} );
 
 const wrapResult = ( block, component ) => {
