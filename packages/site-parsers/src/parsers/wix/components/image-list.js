840f25532c1c6236158c1d1214b72d1876975c1e
Bogdan <bogdan.nikolic87@gmail.com>
2021-06-15 11:24:32 +0200

    Introduce ImageList component mapper (#59)
    Supported galleries:
    - 3DCarousel
    - Slider
    - Masonry
    - Paginated
    - Honeycomb
    - Matrix
    - Paginated grid

---
+++ packages/site-parsers/src/parsers/wix/components/image-list.js
@@ -0,0 +1,51 @@
+const { createBlock } = require( '@wordpress/blocks' );
+
+const parseImages = ( images, metaData ) => {
+	return images.map( ( img ) => {
+		const prefix = metaData.serviceTopology.staticAudioUrl;
+
+		const attrs = {
+			id: img.id,
+			alt: img.alt,
+			title: img.title,
+			caption: img.description,
+			url: prefix + '/' + img.uri,
+			fulUrl: '/' + prefix + '/' + img.uri,
+		};
+
+		if ( img.link ) {
+			attrs.link = img.link.url;
+		}
+
+		return attrs;
+	} );
+};
+
+module.exports = {
+	type: 'ImageList',
+	parseComponent: ( component, { metaData } ) => {
+		const images = parseImages( component.dataQuery.items, metaData );
+		const attrs = {
+			images,
+			ids: images.map( ( img ) => img.id ),
+		};
+
+		switch ( component.propertyQuery.type ) {
+			// Gallery: 3DCarousel, 3DCarousel, Slider Galleries
+			case 'FreestyleProperties':
+			case 'SlideShowGalleryProperties':
+				return createBlock( 'core/gallery', attrs );
+
+			// Gallery: Grid
+			case 'MasonryProperties':
+			case 'HoneycombProperties':
+			case 'MatrixGalleryProperties':
+			case 'PaginatedGridGalleryProperties':
+				attrs.columns = component.propertyQuery.numCols || 3;
+				return createBlock( 'core/gallery', attrs );
+
+			// Gallery: Currently unsupported
+			case 'ImpressProperties':
+		}
+	},
+};
