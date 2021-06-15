74ed33e55c4466ea98bf4480addaba8dfee8a197
Bogdan <bogdan.nikolic87@gmail.com>
2021-06-15 11:06:44 +0200

    Introduce Audio component mapper (#55)

--- packages/site-parsers/src/parsers/wix/mappers.js
+++ packages/site-parsers/src/parsers/wix/mappers.js
@@ -15,6 +15,7 @@ const componentHandlers = [
 	require( './components/menu.js' ),
 	require( './components/image.js' ),
 	require( './components/button.js' ),
+	require( './components/audio.js' ),
 	require( './components/video.js' ),
 ].reduce( handlerMapper( 'type' ), {} );
 
