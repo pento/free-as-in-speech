223c2010e67b8b3a49078bb647dfd16772e1d54f
Bogdan <bogdan.nikolic87@gmail.com>
2021-06-25 11:43:27 +0200

    Fix broken mappers (#69)
    * Adjust AudioComponent attributes mapping
    
    * Adjust VideoComponent attributes mapping
    
    * Separator: add a missing return statement

--- packages/site-parsers/src/parsers/wix/components/video.js
+++ packages/site-parsers/src/parsers/wix/components/video.js
@@ -1,39 +1,24 @@
 const { createBlock } = require( '@wordpress/blocks' );
 
-const getVideoEmbedUrl = ( settings ) => {
-	switch ( settings.videoType ) {
-		case 'YOUTUBE':
-			return `https://www.youtube.com/watch?v=${ settings.videoId }`;
-		case 'VIMEO':
-			return `https://player.vimeo.com/video/${ settings.videoId }`;
-		case 'DAILYMOTION':
-			return `https://www.dailymotion.com/embed/video/${ settings.videoId }`;
-		case 'FACEBOOK':
-			return `https://www.facebook.com/${ settings.videoId }`;
-		default:
-			return '';
-	}
-};
-
 module.exports = {
-	type: 'Video',
+	type: 'VideoPlayer',
 	parseComponent: ( component ) => {
-		const attrs = {
-			src: getVideoEmbedUrl( component.dataQuery ),
-		};
-
-		if (
-			component.propertyQuery &&
-			component.propertyQuery.type === 'VideoProperties'
-		) {
-			Object.assign( attrs, {
-				loop: component.propertyQuery.loop,
-				autoplay: component.propertyQuery.autoplay,
-				controls:
-					component.propertyQuery.showControls !== 'always_hide',
-			} );
+		if ( ! component.dataQuery.videoUrl ) {
+			return null;
 		}
 
-		return createBlock( 'core/video', attrs );
+		switch ( component.dataQuery.videoType.toUpperCase() ) {
+			case 'VIMEO':
+			case 'YOUTUBE':
+			case 'FACEBOOK':
+			case 'DAILYMOTION':
+				return createBlock( 'core/embed', {
+					url: component.dataQuery.videoUrl,
+					providerNameSlug: component.dataQuery.videoType,
+				} );
+
+			default:
+				return null;
+		}
 	},
 };
